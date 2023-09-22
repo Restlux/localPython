50c4e83dd0af19e82b74e9bf261bcafe78eac9e6
Restlux <2020.ironwolf.2020@gmail.com>
2023-09-22 13:43:20 +0530

    Made nameImport more compact

--- nameImport.py
+++ nameImport.py
@@ -1,8 +1,3 @@
-names = []
-
-with open("names.txt") as file:
+with open ("names.txt", "r") as file:
     for line in file:
-        names.append(line.rstrip())
-
-for name in sorted(names):
-    print(f"hello, {name} ")
+        print("hello,",line.rstrip())
